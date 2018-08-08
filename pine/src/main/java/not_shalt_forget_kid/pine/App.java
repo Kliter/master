83e420c17a340d0a609005fa9d19e4d7c0d6cedf
Katsumi Kusumi <katsumikusumi@KatsuminoMacBook-Pro.local>
2018-08-08 22:59:59 +0900

    create project

---
+++ pine/src/main/java/not_shalt_forget_kid/pine/App.java
@@ -0,0 +1,13 @@
+package not_shalt_forget_kid.pine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
